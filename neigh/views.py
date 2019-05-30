d5a906c44e8b4d0fc08bbf828e4c0707d774b76d
OkothPius <orukopius8@gmail.com>
2019-05-30 21:48:02 +0300

    create routes

--- neigh/views.py
+++ neigh/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'neigh/home.html')
+
+
+def about(request):
+    return render(request, 'neigh/about.html')
