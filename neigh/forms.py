a1e7dae325ecd7f9cecfaad0fe5a8e9249412a8b
OkothPius <orukopius8@gmail.com>
2019-05-31 19:55:03 +0300

    create forms

---
+++ neigh/forms.py
@@ -0,0 +1,36 @@
+from .models import Neighborhood,Business,Profile,Post
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+class NeighborhoodForm(ModelForm):
+    class Meta:
+        model = Neighborhood
+        fields = (
+            'neighborhood_name',
+            )
+
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = (
+            'first_name',
+            'last_name',
+            'location',
+            )
+
+class AddBusinessForm(ModelForm):
+    class Meta:
+        model = Business
+        fields = (
+            'name',
+            'email',
+            'business_location',
+            )
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'post_description',
+            )
