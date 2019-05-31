e77ac1e15c749d79bc98528151c087293cac30a3
OkothPius <orukopius8@gmail.com>
2019-05-31 19:54:43 +0300

    add paths

--- neigh/urls.py
+++ neigh/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url('',views.home, name='homePage'),
-    url(r'^about/',views.home, name='AboutPage'),
+    url(r'^signout/$',views.signout,name='signout'),
+    url(r'^neighborhood/(\d+)',views.neighborhood,name='neighborhood'),
+    url(r'^profile/(\d+)',views.profile,name='profile'),
+    url(r'^add_biz/',views.add_biz,name='add_biz'),
+    url(r'^change_neighborhood/(\d+)',views.change_neighborhood,name='change_neighborhood'),
+    url(r'^search/',views.search,name='search'),
 ]
